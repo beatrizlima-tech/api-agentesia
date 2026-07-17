f4018f5b18d95c3199f228c2c3326f3701bcd060
beatrizlima-tech <bealima.tech@gmail.com>
2026-07-17 10:25:24 -0300

    chore: iniciar estrutura do projeto API AgentesIA

---
+++ src/main/java/br/com/cotiinformatica/api_agentesia/ApiAgentesiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.cotiinformatica.api_agentesia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAgentesiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAgentesiaApplication.class, args);
+	}
+
+}
